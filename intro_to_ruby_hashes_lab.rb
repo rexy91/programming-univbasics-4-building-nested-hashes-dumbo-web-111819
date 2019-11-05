8e69f39b1a81e40d9a74d9ffd1d97332bd0032ac
Yu Hao Ye <rxye1991@gmail.com>
2019-11-05 14:22:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -71,7 +71,7 @@ def monopoly_with_fourth_tier
 	   :names => {
 	   :reading_railroad=>
       {
-          "montage"
+          "mortgage_value"=>"$100"
       },
       :pennsylvania_railroad=>
       {
